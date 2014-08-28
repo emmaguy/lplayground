d6aa4f13a663760a6de1e355a3dd741779c8fe04
Emma Guy <emmag87@gmail.com>
2014-08-28 17:00:43 +0100

    tidied up animations by creating subclasses so unused methods arent taking up space

--- app/src/main/java/emmaguy/l/HierarchicalTimingActivity.java
+++ app/src/main/java/emmaguy/l/HierarchicalTimingActivity.java
@@ -1,12 +1,13 @@
 package emmaguy.l;
 
+import android.animation.Animator;
 import android.app.Activity;
 import android.os.Bundle;
 import android.transition.Transition;
-import android.view.View;
+import android.view.ViewPropertyAnimator;
 
 
-public class HierarchicalTimingActivity extends Activity implements Transition.TransitionListener {
+public class HierarchicalTimingActivity extends Activity {
 
     private static final int NUM_VIEWS = 8;
     private static final int SCALE_ITEM_ANIMATION_DELAY = 30;
@@ -17,36 +18,33 @@ public class HierarchicalTimingActivity extends Activity implements Transition.T
 
         setContentView(R.layout.activity_hierarchical_timing);
 
-        getWindow().getEnterTransition().addListener(this);
+        getWindow().getEnterTransition().addListener(new TransListener() {
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                getWindow().getEnterTransition().removeListener(this);
+
+                for (int i = 0; i < NUM_VIEWS; i++) {
+                    int resID = getResources().getIdentifier("view_" + (i + 1), "id", getPackageName());
+                    findViewById(resID).animate().setStartDelay(i * SCALE_ITEM_ANIMATION_DELAY).scaleX(1).scaleY(1);
+                }
+            }
+        });
     }
 
     @Override
-    public void onTransitionStart(Transition transition) {
-
-    }
-
-    @Override
-    public void onTransitionEnd(Transition transition) {
-        getWindow().getEnterTransition().removeListener(this);
-
+    public void onBackPressed() {
         for (int i = 0; i < NUM_VIEWS; i++) {
             int resID = getResources().getIdentifier("view_" + (i + 1), "id", getPackageName());
-            findViewById(resID).animate().setStartDelay(i * SCALE_ITEM_ANIMATION_DELAY).scaleX(1).scaleY(1);
+
+            ViewPropertyAnimator viewPropertyAnimator = findViewById(resID).animate().setStartDelay(i * SCALE_ITEM_ANIMATION_DELAY).scaleX(0).scaleY(0);
+            if (i + 1 >= NUM_VIEWS) {
+                viewPropertyAnimator.setListener(new AnimListener() {
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+                        finishAfterTransition();
+                    }
+                });
+            }
         }
-    }
-
-    @Override
-    public void onTransitionCancel(Transition transition) {
-
-    }
-
-    @Override
-    public void onTransitionPause(Transition transition) {
-
-    }
-
-    @Override
-    public void onTransitionResume(Transition transition) {
-
     }
 }
---
+++ app/src/main/java/emmaguy/l/TransListener.java
@@ -0,0 +1,30 @@
+package emmaguy.l;
+
+import android.transition.Transition;
+
+public class TransListener implements Transition.TransitionListener {
+    @Override
+    public void onTransitionStart(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionEnd(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionCancel(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionPause(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionResume(Transition transition) {
+
+    }
+}
